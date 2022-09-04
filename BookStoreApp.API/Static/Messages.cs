78033695c1aadb8cc04b16dbeb0bb844fda85e01
lisa-crowell <lisa.l.crowell@gmail.com>
2022-09-04 14:10:26 -0500

    add messages

---
+++ BookStoreApp.API/Static/Messages.cs
@@ -0,0 +1,6 @@
+namespace BookStoreApp.API.Static;
+
+public class Messages
+{
+    public const string Error500Message = "An error occurred while processing your request. Please try again later.";
+}
