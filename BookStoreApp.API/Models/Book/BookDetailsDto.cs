18ca73f19e182c8232e29ab841b0fa33958c031a
lisa-crowell <lisa.l.crowell@gmail.com>
2022-09-05 21:07:34 -0500

    create book dtos

---
+++ BookStoreApp.API/Models/Book/BookDetailsDto.cs
@@ -0,0 +1,14 @@
+namespace BookStoreApp.API.Models.Book;
+
+public class BookDetailsDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public string ISBN { get; set; }
+    public string Summary { get; set; }
+    public string Image { get; set; }
+    public decimal Price { get; set; }
+    public int AuthorId { get; set; }
+    public string AuthorName { get; set; }
+}
---
+++ BookStoreApp.API/Models/Book/BookUpdateDto.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApp.API.Models.Book;
+
+public class BookUpdateDto : BaseDto
+{
+    [Required]
+    [StringLength(55)]
+    public string? Title { get; set; }
+    [Required]
+    public string Isbn { get; set; }
+    [Required]
+    [StringLength(255), MinLength(10)]
+    public string? Summary { get; set; }
+    
+    public string? Image { get; set; }
+    [Required]
+    [Range(0, int.MaxValue)]
+    public decimal? Price { get; set; }
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int? Year { get; set; }
+}
