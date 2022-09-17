d4519bcd077dcf7c30b46ada5b3591d5b53e8616
lisa-crowell <lisa.l.crowell@gmail.com>
2022-09-17 17:31:08 -0500

    add authentication

---
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,8 @@
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.Server.UI.Services.Authentication;
+
+public interface IAuthenticationService
+{
+    Task<bool> AuthenticateAsync(LoginUserDto loginModel); 
+}
