861aaa8dab8d1b9419375e1dce87a09828f01282
lisa-crowell <lisa.l.crowell@gmail.com>
2022-09-20 08:17:50 -0500

    add author interface and class

---
+++ BookStoreApp.Blazor.Server.UI/Services/AuthorService.cs
@@ -0,0 +1,36 @@
+using Blazored.LocalStorage;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.Server.UI.Services;
+
+public class AuthorService : BaseHttpService, IAuthorService
+{
+    private readonly IClient _client;
+    
+    public AuthorService(IClient client, ILocalStorageService localStorageService) : base(client, localStorageService)
+    {
+        _client = client;
+    }
+
+    public async Task<Response<List<AuthorReadOnlyDto>>> GetAuthors()
+    {
+        Response<List<AuthorReadOnlyDto>> response;
+
+        try
+        {
+            await GetBearerToken();
+            var data = await _client.AuthorsAllAsync();
+            response = new Response<List<AuthorReadOnlyDto>>
+            {
+                Data = data.ToList(),
+                IsSuccess = true
+            };
+        }
+        catch (ApiException exception)
+        {
+            response = ConvertApiExceptions<List<AuthorReadOnlyDto>>(exception);
+        }
+
+        return response;
+    }
+}
---
+++ BookStoreApp.Blazor.Server.UI/Services/IAuthorService.cs
@@ -0,0 +1,8 @@
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.Server.UI.Services;
+
+public interface IAuthorService
+{
+    Task<Response<List<AuthorReadOnlyDto>>> GetAuthors();
+}
