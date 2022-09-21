59fcd064094e46b857410ea374cea8bf934d11dc
lisa-crowell <lisa.l.crowell@gmail.com>
2022-09-21 09:27:31 -0500

    add edit author functionality

--- BookStoreApp.Blazor.Server.UI/Services/AuthorService.cs
+++ BookStoreApp.Blazor.Server.UI/Services/AuthorService.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Blazored.LocalStorage;
 using BookStoreApp.Blazor.Server.UI.Services.Base;
 
@@ -6,13 +7,14 @@ namespace BookStoreApp.Blazor.Server.UI.Services;
 public class AuthorService : BaseHttpService, IAuthorService
 {
     private readonly IClient _client;
-    
-    public AuthorService(IClient client, ILocalStorageService localStorageService) : base(client, localStorageService)
+    private readonly IMapper _mapper;
+    public AuthorService(IClient client, ILocalStorageService localStorageService, IMapper mapper) : base(client, localStorageService)
     {
         _client = client;
+        _mapper = mapper;
     }
 
-    public async Task<Response<List<AuthorReadOnlyDto>>> GetAuthors()
+    public async Task<Response<List<AuthorReadOnlyDto>>> GetAuthor()
     {
         Response<List<AuthorReadOnlyDto>> response;
 
@@ -34,6 +36,50 @@ public class AuthorService : BaseHttpService, IAuthorService
         return response;
     }
 
+    public async Task<Response<AuthorReadOnlyDto>> GetAuthor(int id)
+    {
+        Response<AuthorReadOnlyDto> response;
+
+        try
+        {
+            await GetBearerToken();
+            var data = await _client.AuthorsGETAsync(id);
+            response = new Response<AuthorReadOnlyDto>
+            {
+                Data = data,
+                IsSuccess = true
+            };
+        }
+        catch (ApiException exception)
+        {
+            response = ConvertApiExceptions<AuthorReadOnlyDto>(exception);
+        }
+
+        return response;
+    }
+
+    public async Task<Response<AuthorUpdateDto>> GetAuthorForUpdate(int id)
+    {
+        Response<AuthorUpdateDto> response;
+
+        try
+        {
+            await GetBearerToken();
+            var data = await _client.AuthorsGETAsync(id);
+            response = new Response<AuthorUpdateDto>
+            {
+                Data = _mapper.Map<AuthorUpdateDto>(data),
+                IsSuccess = true
+            };
+        }
+        catch (ApiException exception)
+        {
+            response = ConvertApiExceptions<AuthorUpdateDto>(exception);
+        }
+
+        return response;
+    }
+
     public async Task<Response<int>> CreateAuthor(AuthorCreateDto author)
     {
         Response<int> response = new();
@@ -41,6 +87,23 @@ public class AuthorService : BaseHttpService, IAuthorService
         {
             await GetBearerToken();
             await _client.AuthorsPOSTAsync(author);
+        }
+        catch (ApiException exception)
+        {
+            response = ConvertApiExceptions<int>(exception);
+        }
+
+        return response;
+    }
+
+    public async Task<Response<int>> EditAuthor(int id, AuthorUpdateDto author)
+    {
+        Response<int> response = new();
+
+        try
+        {
+            await GetBearerToken();
+            await _client.AuthorsPUTAsync(id, author);
         }
         catch (ApiException exception)
         {
