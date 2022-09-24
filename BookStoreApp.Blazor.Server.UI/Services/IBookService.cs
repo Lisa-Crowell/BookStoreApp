b04937029fc3658f4267ca24fcbe659b32adf6f1
lisa-crowell <lisa.l.crowell@gmail.com>
2022-09-24 17:09:13 -0500

    add book service interface

---
+++ BookStoreApp.Blazor.Server.UI/Services/IBookService.cs
@@ -0,0 +1,13 @@
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.Server.UI.Services;
+
+public interface IBookService
+{
+    Task<Response<List<BookReadOnlyDto>>> GetBook();
+    Task<Response<BookDetailsDto>> GetBook(int id);
+    Task<Response<BookUpdateDto>> GetBookForUpdate();
+    Task<Response<int>> CreateBook(BookCreateDto bookCreateDto);
+    Task<Response<int>> EditBook(int id, BookUpdateDto bookUpdateDto);
+    Task<Response<int>> DeleteBook(int id);
+}
